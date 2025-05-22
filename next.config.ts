19f70e95c56b7b805697f79f388615bb42aa4211
Yonatan Hidson Simbolon <jonatanhudson.s@gmail.com>
2025-05-22 14:45:50 +0700

    starter jinmu-signature

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+    // output: 'export',
+    // images: {
+    //     unoptimized: true,
+    // },
+    // distDir: 'out',
+    // trailingSlash: true,
+    // assetPrefix: '.',
+
+
+    // staticPageGenerationTimeout: 1000,
+    // reactStrictMode: false
 };
 
 export default nextConfig;
